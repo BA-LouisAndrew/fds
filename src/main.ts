bdb90a1880058477a9340f62c510d90b464a57a0
Louis Andrew <louisandrew3@gmail.com>
2022-05-08 15:15:26 +0200

    feat(express): complete eslint and express middleware setup

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
-import express, { Request, Response } from 'express';
+import cors from "cors"
+import express, { Request, Response } from "express"
 
-const app = express();
+const app = express()
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Application works!');
-});
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Application works!")
+})
 
 app.listen(3000, () => {
-  console.log('Application started on port 3000!');
-});
+  console.log("Application started on port 3000!")
+})
