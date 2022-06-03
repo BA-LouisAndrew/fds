ab0f4bf1b3d3033104a3e8c7fd14f2aba02a40c2
Louis Andrew <louisandrew3@gmail.com>
2022-06-03 09:58:53 +0200

    feat(evaluator): add function to have value as an access path to object

--- src/engine/condition/conditionEvaluator.ts
+++ src/engine/condition/conditionEvaluator.ts
@@ -4,16 +4,18 @@ import { Condition } from "@/types/rule"
 import { Evaluator } from "./evaluator"
 
 export class ConditionEvaluator extends Evaluator<Condition> {
-  runEvaluation(data: any): void {
+  protected runEvaluation(data: any): void {
     const { path, operator: operatorId, type, value, failMessage } = this.condition
     const dataFromPath = this.accessDataFromPath(data, path)
+    const valueFromPath = this.accessDataFromPath(data, value)
+
     if (dataFromPath === null || dataFromPath === undefined) {
       this.result.pass = false
       return
     }
 
     const operator = OperatorFactory.getOperator(type, operatorId)
-    const isValid = operator.operate(value, dataFromPath)
+    const isValid = operator.operate(valueFromPath, dataFromPath)
 
     if (!isValid) {
       this.result.messages.push(this.formatMessage(failMessage, dataFromPath))
